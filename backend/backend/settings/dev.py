9e169d7215a7e16adec593b779dafc32d50648c9
shivaramkumar <connect.shivaram@gmail.com>
2024-10-24 18:31:06 +0000

    working copy

---
+++ backend/backend/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-07ulfeuq3+^zfd=tdjynb1c=y^(f4o(9z$@5w4_i23+2s=1+1k"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
